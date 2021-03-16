7431c9c732b472de595818c3749aeba380c00332
Ahmad Alif Sofian <alif12sofian@gmail.com>
2021-03-16 08:15:28 +0700

    Frontend: Add footer ,modal contact for footer and change route in web.php

---
+++ resources/views/test/modalcontact.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="testss" tabindex="-1" role="dialog" aria-labelledby="testssLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="testssLabel">Whatsapp Contact</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <li style="list-style-type: none;">Admin 1 : <a style="float:right" class="btn btn-success btn-sm rounded-pill " href="https://api.whatsapp.com/send/?phone=6282289229300&text=Halo+Kak%2C+saya+mau+order%0A%0ANama+%3A%0AAlamat+lunch+%3A%0AAlamat+dinner+%3A%0ANo+HP+%3A%0AMenu+%3A+Lunch+%2F+Dinner%0A+++++++++++++Weekly+%2F+Daily%0A%0Aps+%3A+order+maksimal+H-1+ya+kak%0A%0ATerimakasih&app_absent=0" target="_blank">Contact</a></li>
+                </br>
+                <li style="list-style-type: none;">Admin 2 : <a style="float:right" class="btn btn-success btn-sm rounded-pill " href="https://api.whatsapp.com/send/?phone=6282289229300&text=Halo+Kak%2C+saya+mau+order%0A%0ANama+%3A%0AAlamat+lunch+%3A%0AAlamat+dinner+%3A%0ANo+HP+%3A%0AMenu+%3A+Lunch+%2F+Dinner%0A+++++++++++++Weekly+%2F+Daily%0A%0Aps+%3A+order+maksimal+H-1+ya+kak%0A%0ATerimakasih&app_absent=0" target="_blank">Contact</a></li>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
