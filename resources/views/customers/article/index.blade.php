d533e2204244d5da2a1a41d3d0e5ccd7cecdff6f
alifahmaaad <ahmad.118140133@student.itera.ac.id>
2021-04-11 20:05:15 +0700

    update article page and add show article

--- resources/views/customers/article/index.blade.php
+++ resources/views/customers/article/index.blade.php
@@ -5,26 +5,42 @@
 
         <div class="container faq  mb-2 ">
             <h1>Article</h1>
-            <div class="row featurette">
-                <div class="col-md-7 order-md-1">
-                    <p>judul</p>
-                    <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-                    <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+            <div class="row ">
+                <div class="col-md-9 order-md-2 ">
+                    <h2>Judul</h2>
+                    <p class="lead hiddentext">Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.
+                    </p>
+                    <p class="text-center link readmore"><a href="" class="link">Read More...</a></p>
                 </div>
-                <div class="col-md-5 order-md-2">
-                    <img class="bd-placeholder-img bd-placeholder-img-lg image img-fluid mx-auto responsive" width="500" height="500" src="{{ asset('assets/images/image1.jpg') }}" alt="">
+                <div class="col-md-3 order-md-2">
+                    <img class="bd-placeholder-img bd-placeholder-img-lg image img-fluid mx-auto responsive" width="250" height="250" src="{{ asset('assets/images/image1.jpg') }}" alt="">
 
                 </div>
             </div>
             <hr class="featurette-divider">
-            <div class="row featurette">
-                <div class="col-md-7 order-md-2">
-                    <p>judul</p>
-                    <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-                    <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+            <div class="row ">
+                <div class="col-md-9 order-md-2 ">
+                    <h2>Judul</h2>
+                    <p class="lead hiddentext">Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.Another featurette? Of course. More placeholder content here to give
+                        you an idea of how this layout would work with some actual real-world content in place.
+                    </p>
+                    <p class="text-center link readmore"><a href="" class="link">Read More...</a></p>
                 </div>
-                <div class="col-md-5 order-md-1">
-                    <img class="bd-placeholder-img bd-placeholder-img-lg image img-fluid mx-auto responsive" width="500" height="500" src="{{ asset('assets/images/image1.jpg') }}" alt="">
+                <div class="col-md-3 order-md-1">
+                    <img class="bd-placeholder-img bd-placeholder-img-lg image img-fluid mx-auto responsive" width="250" height="250" src="{{ asset('assets/images/image1.jpg') }}" alt="">
 
                 </div>
             </div>
---
+++ resources/views/customers/article/showarticle.blade.php
@@ -0,0 +1,7 @@
+@include('templates.customers.header')
+<div class="container" style="padding-top: 100px;">
+    <img style="float: left;margin: 0px 15px 15px 0px;" width="350" src=" {{ asset('assets/images/image1.jpg') }}" class="imgswarticle">
+    <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
+    <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+</div>
+@include('templates.customers.footer')
