15f545b14340250af21ab8a9fa82515c95564c9f
alifahmaaad <ahmad.118140133@student.itera.ac.id>
2021-04-15 05:50:07 +0700

    update home page and delete dummy image

--- resources/views/customers/home/index.blade.php
+++ resources/views/customers/home/index.blade.php
@@ -1,5 +1,5 @@
 @include('templates.customers.header')
-<div class="paddingheader" style="background-image: url({{ asset('assets/images/bghomepage.jpg') }});background-size: 100% 100%;background-repeat: no-repeat;">
+<div class="paddingheader" style="background-image: linear-gradient(180deg, #f1bbbc 80%, white);background-size: 100% 100%;background-repeat: no-repeat;">
     <main class="container pt-5">
         <div id="myCarousel" class="carousel slide py-5" data-ride="carousel">
             </br>
@@ -13,27 +13,21 @@
                     <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
                         <rect width="100%" height="100%" fill="#fff" />
                     </svg>
-                    <img class="responsive" src="https://png.pngtree.com/thumb_back/fh260/background/20190223/ourmid/pngtree-pure-color-watercolor-graffiti-gradient-background-board-design-board-design-image_66713.jpg" alt=""></img>
-
-                    <div class="container">
+                    <img class="responsive" src="{{ asset('assets/images/urdailyhealth.png') }}" alt=""></img>
+                    <!-- <div class="container">
                         <div class="carousel-caption text-start">
                             <h2>Example headline.</h2>
                             <p>Some representative placeholder content for the first slide of the carousel.</p>
                             <p><a class="btn btn-md btn-primary" href="#">Sign up today</a></p>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
                 <div id="headcarousel" class="carousel-item">
                     <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
                         <rect width="100%" height="100%" fill="#fff" />
                     </svg>
-                    <img class="responsive" src="{{ asset('assets/images/logo.jpg') }}" alt="">
+                    <img class="responsive" src="" alt="">
                     <div class="container">
-                        <div class="carousel-caption">
-                            <h2>Another example headline.</h2>
-                            <p>Some representative placeholder content for the second slide of the carousel.</p>
-                            <p><a class="btn btn-md btn-primary" href="#">Learn more</a></p>
-                        </div>
                     </div>
                 </div>
                 <div id="headcarousel" class="carousel-item ">
@@ -41,13 +35,6 @@
                         <rect width="100%" height="100%" fill="#fff" />
                     </svg>
 
-                    <div class="container">
-                        <div class="carousel-caption text-end">
-                            <h2>One more for good measure.</h2>
-                            <p>Some representative placeholder content for the third slide of this carousel.</p>
-                            <p><a class="btn btn-md btn-primary " href="#">Browse gallery</a></p>
-                        </div>
-                    </div>
                 </div>
             </div>
             <a class="carousel-control-prev" role="button" href="#myCarousel" data-slide="prev">
@@ -70,18 +57,24 @@
 
         <div class="row ">
             <div class="col-lg-6 text-center">
-                <img class="bd-placeholder-img " width="200" height="200" src="{{ asset('assets/images/logo.jpg') }}" alt="">
-                <h2 class="mt-4">Lunch</h2>
-                <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-                <p><a class="btn btn-secondary" href="pricelist">See Menu</a> <a class="btn btn-secondary" href="#">Order Now &raquo;</a> </p>
+                <img class="bd-placeholder-img border rounded" width="200" height="200" src="{{ asset('assets/images/urdailyhealth.png') }}" alt="">
+                <h2 class="mt-4">Tomorrow Lunch</h2>
+                @foreach($lunchs as $lunch)
+                <p>{{ $lunch->description }}</p>
+                <p>@currency($lunch->price )</p>
+                @endforeach
+                <p><a class="btn btn-secondary" href="pricelist">See Menu</a> <a class="btn btn-secondary" href="#!" role="button" data-toggle="modal" data-target="#ordermodal">Order Now &raquo;</a> </p>
             </div>
 
             <div class="col-lg-6 text-center ">
 
-                <img class="bd-placeholder-img " width="200" height="200" src="{{ asset('assets/images/logo.jpg') }}" alt="">
-                <h2 class="mt-4">Dinner</h2>
-                <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-                <p><a class="btn btn-secondary" href="pricelist">See Menu</a> <a class="btn btn-secondary" href="#">Order Now &raquo;</a> </p>
+                <img class="bd-placeholder-img border rounded" width="200" height="200" src="{{ asset('assets/images/urdailyhealth.png') }}" alt="">
+                <h2 class="mt-4">Tomorrow Dinner</h2>
+                @foreach($dinners as $dinner)
+                <p>{{ $dinner->description }}</p>
+                <p>@currency($dinner->price)</p>
+                @endforeach
+                <p><a class="btn btn-secondary" href="pricelist">See Menu</a> <a class="btn btn-secondary" href="#!" role="button" data-toggle="modal" data-target="#ordermodal">Order Now &raquo;</a> </p>
             </div>
         </div>
 
@@ -89,72 +82,43 @@
         <!-- START THE FEATURETTES -->
 
         <hr class="featurette-divider">
-
+        @foreach($articles as $article)
+        @if($loop->odd )
         <div class="row featurette">
-            <div class="col-md-7">
-                <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-                <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+            <div class="col-md-8 order-md-1">
+                <h2 class="featurette-heading">{{ $article->title }}</h2>
+                <p class="lead">{{ $article->content }}</p>
             </div>
-            <div class="col-md-5">
-                <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-                    </svg> -->
-                <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel" data-interval="3000">
-                    <div id="imgarticle" class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto responsive" width="500" height="500" src="{{ asset('assets/images/image1.jpg') }}" alt="">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto responsive" width="500" height="500" src="{{ asset('assets/images/image1.jpg') }}" alt="">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto responsive" width="500" height="500" src="{{ asset('assets/images/image1.jpg') }}" alt="">
-                        </div>
-                    </div>
-                </div>
-
+            <div class="col-md-4 order-md-2">
+                <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto responsive" src="{{ asset($article->thumbnail) }}" alt="">
             </div>
         </div>
 
         <hr class="featurette-divider">
-
+        @else
         <div class="row featurette">
-            <div class="col-md-7 order-md-2">
-                <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-                <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+            <div class="col-md-8 order-md-2">
+                <h2 class="featurette-heading">{{ $article->title }}</h2>
+                <p class="lead">{{ $article->content }}</p>
             </div>
-            <div class="col-md-5 order-md-1">
-                <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-                    </svg> -->
-                <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto responsive" width="500" height="500" src="{{ asset('assets/images/image1.jpg') }}" alt="">
-
+            <div class="col-md-4 order-md-1">
+                <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto responsive" src="{{ asset($article->thumbnail) }}" alt="">
             </div>
         </div>
-
         <hr class="featurette-divider">
+        @endif
+        @endforeach
 
         <p class="text-center featurette-heading my-0 headertesti">Testimoni</p>
         <div id="Testimoni" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner testicontent">
-                <div class="carousel-item active">
-                    <h2 class="testitittle">Oomaygaddddd adawdawdaw <span class="text-muted">See for yourself.</span></h2>
+                @foreach($testi as $testimoni)
+                <div class="carousel-item ">
+                    <h2 class="testitittle">{{$testimoni->nama}}</h2>
                     <p class="captiontesti">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
 
                 </div>
-                <div class="carousel-item">
-                    <h2 class="testitittle">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-                    <p class="captiontesti">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
-
-                </div>
-                <div class=" carousel-item">
-                    <h2 class="testitittle">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-                    <p class="captiontesti">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
-
-                </div>
-
+                @endforeach
             </div>
             <a class="carousel-control-prev " href="#Testimoni" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
