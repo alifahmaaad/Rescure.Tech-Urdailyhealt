cae1a04502f7bcc06c99400404ca054b1c1c294f
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2021-04-20 08:56:40 +0700

    update img src

--- resources/views/templates/administrators/adminlte.blade.php
+++ resources/views/templates/administrators/adminlte.blade.php
@@ -70,7 +70,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <aside class="main-sidebar sidebar-light-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{ url('/') }}" class="brand-link">
-                <img src="{{ asset('assets/images/logo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="{{ asset('assets/images/urdailyhealth.png') }}" alt="urdailyhealth logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Urdaily Health</span>
             </a>
 
